99be3e74c814bb39662e9879ee70117d157c39b6
oocho0 <rlawngml59@gmail.com>
2024-11-05 21:42:07 +0900

    init

---
+++ src/test/java/com/application/soothee/SootheeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.application.soothee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SootheeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
