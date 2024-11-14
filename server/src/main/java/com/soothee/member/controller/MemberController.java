59f2dbdce946a0bc37f33ffaf343d8fffee566fe
JuHee Kim <rlawngml59@gmail.com>
2024-11-14 23:56:15 +0900

    Feat: MVC 구조 설정 (#11)
    * Feat: reference entity 분리
    
    * Feat: 필드 수정 불가 설정 및 entity 수정 삭제 method 생성
    
    * Feat: MVC interface & class 생성

---
+++ server/src/main/java/com/soothee/member/controller/MemberController.java
@@ -0,0 +1,9 @@
+package com.soothee.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+}
---
+++ server/src/main/java/com/soothee/reference/service/WeatherService.java
@@ -0,0 +1,4 @@
+package com.soothee.reference.service;
+
+public interface WeatherService {
+}
