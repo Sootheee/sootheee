109c3874e789e0d13d9a945c06aa78ce83dd37a3
Juhee Kim <rlawngml59@gmail.com>
2024-11-25 17:25:23 +0900

    Feat: API 명세 작성

--- server/src/main/java/com/soothee/member/controller/MemberController.java
+++ server/src/main/java/com/soothee/member/controller/MemberController.java
@@ -5,6 +5,12 @@ import com.soothee.member.domain.Member;
 import com.soothee.member.dto.UpdateMemberDTO;
 import com.soothee.member.service.MemberService;
 import io.micrometer.common.util.StringUtils;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +30,15 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/update")
+    @Operation(summary = "회원의 현재 닉네임", description = "회원 닉네임 변경 세팅 창에서 닉네임을 바꾸기 전에 현재 닉네임을 먼저 출력")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(mediaType = "application/json")),
+        @ApiResponse(responseCode = "404", description = "회원 정보 없음", content = @Content(mediaType = "text/plain"))
+    })
+    @Parameters({
+            @Parameter(name = "id", description = "회원 고유일련번호", example = "1112"),
+            @Parameter(name = "member_name", description = "회원 닉네임", example = "사용자")
+    })
     public ResponseEntity<?> updateMemberName(Principal principal) {
         String loginId = principal.getName();
         Member loginMember = memberService.getMemberByEmail(loginId);
