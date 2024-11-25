2e0c6c09810cf6bb200ec53e7d65dab9cc5f91a3
Juhee Kim <rlawngml59@gmail.com>
2024-11-25 22:19:18 +0900

    Fix: Swagger Security 설정 추가

--- server/src/main/java/com/soothee/member/controller/MemberController.java
+++ server/src/main/java/com/soothee/member/controller/MemberController.java
@@ -11,6 +11,7 @@ import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -30,7 +31,8 @@ public class MemberController {
     private final MemberService memberService;
 
     @GetMapping("/update")
-    @Operation(summary = "회원의 현재 닉네임", description = "회원 닉네임 변경 세팅 창에서 닉네임을 바꾸기 전에 현재 닉네임을 먼저 출력")
+    @Operation(summary = "회원의 현재 닉네임", description = "회원 닉네임 변경 세팅 창에서 닉네임을 바꾸기 전에 현재 닉네임을 먼저 출력",
+            security = @SecurityRequirement(name = "oauth2_auth"))
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "요청 성공", content = @Content(mediaType = "application/json")),
         @ApiResponse(responseCode = "404", description = "회원 정보 없음", content = @Content(mediaType = "text/plain"))
