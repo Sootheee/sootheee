06ea8f4388cdb4890136f3d74c7730f3cc6f66a8
Juhee Kim <rlawngml59@gmail.com>
2024-11-25 11:34:53 +0900

    Chore: transaction annotation 추가

--- server/src/main/java/com/soothee/oauth2/service/DelegatingOAuth2Service.java
+++ server/src/main/java/com/soothee/oauth2/service/DelegatingOAuth2Service.java
@@ -6,11 +6,13 @@ import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class DelegatingOAuth2Service extends DefaultOAuth2UserService {
     private final List<CustomOAuth2UserService> oauth2UserServices;
 
