c039ce0c4cb8d789420130ef1624e355ba313a9b
Juhee Kim <rlawngml59@gmail.com>
2024-11-25 22:57:40 +0900

    Feat: Custom Error Msg 추가

--- server/src/main/java/com/soothee/common/exception/MyErrorMsg.java
+++ server/src/main/java/com/soothee/common/exception/MyErrorMsg.java
@@ -5,7 +5,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum MyErrorMsg {
     NOT_EXIST_MEMBER("해당 정보로 조회된 회원이 없습니다."),
-    NULL_VALUE("값이 없습니다.");
+    NULL_VALUE("값이 없습니다."),
+    MISS_MATCH_MEMBER("로그인한 회원의 정보가 아닙니다.");
 
     private final String msg;
 
