b9c701bded14404a398f8cc205106955bc4b198f
Juhee Kim <rlawngml59@gmail.com>
2024-11-28 16:50:33 +0900

    Chore: JPQL 코드 변경

--- server/src/main/java/com/soothee/dairy/repository/DairyRepository.java
+++ server/src/main/java/com/soothee/dairy/repository/DairyRepository.java
@@ -12,7 +12,6 @@ import java.util.Optional;
 
 @Repository
 public interface DairyRepository extends JpaRepository<Dairy, Long> {
-
     /**
      * 로그인한 계정이 지정한 년도-월에 작성한 일기 정보 조회
      * @param memberId Long : 로그인한 계정 일련번호
@@ -20,13 +19,14 @@ public interface DairyRepository extends JpaRepository<Dairy, Long> {
      * @param month Integer : 지정한 월
      * @return Optional<List<MonthlyDairyScoreDTO>> : 조회된 (일기 일련번호, 일기 날짜, 오늘의 점수) 정보 리스트
      */
-    @Query(value = "SELECT new com.soothee.dairy.dto.MonthlyDairyScoreDTO(dairy.id, dairy.date, dairy.score) " +
-            "FROM Dairy dairy " +
-            "WHERE dairy.id = :member_id " +
-            "AND YEAR(dairy.date) = :year " +
-            "AND MONTH(dairy.date) = :month")
+    @Query(value = "SELECT new com.soothee.dairy.dto.MonthlyDairyScoreDTO(d.dairyId, d.date, d.score)" +
+            "FROM Dairy d " +
+            "WHERE d.member = :member_id " +
+            "AND YEAR(d.date) = :year " +
+            "AND MONTH(d.date) = :month")
     Optional<List<MonthlyDairyScoreDTO>> findByMemberIdAndDate(@Param("member_id") Long memberId,
                                                                @Param("year") Integer year,
                                                                @Param("month") Integer month);
 
+    List<Dairy> findByMemberMemberId(Long memberId);
 }
