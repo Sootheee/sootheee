dc5df6186dbc8c466ac73957292d2d5ae8b15a90
Juhee Kim <rlawngml59@gmail.com>
2024-11-28 11:53:41 +0900

    Chore: 일기 리포지토리 메소드 주석 추가

--- server/src/main/java/com/soothee/dairy/repository/DairyRepository.java
+++ server/src/main/java/com/soothee/dairy/repository/DairyRepository.java
@@ -13,6 +13,13 @@ import java.util.Optional;
 @Repository
 public interface DairyRepository extends JpaRepository<Dairy, Long> {
 
+    /**
+     * 로그인한 계정이 지정한 년도-월에 작성한 일기 정보 조회
+     * @param memberId Long : 로그인한 계정 일련번호
+     * @param year Integer : 지정한 년도
+     * @param month Integer : 지정한 월
+     * @return Optional<List<MonthlyDairyScoreDTO>> : 조회된 (일기 일련번호, 일기 날짜, 오늘의 점수) 정보 리스트
+     */
     @Query(value = "SELECT new com.soothee.dairy.dto.MonthlyDairyScoreDTO(dairy.id, dairy.date, dairy.score) " +
             "FROM Dairy dairy " +
             "WHERE dairy.id = :member_id " +
