73e7f3ed79df54449c19e1b520ef7c524f6ba483
Juhee Kim <rlawngml59@gmail.com>
2024-11-29 16:44:20 +0900

    Test: Dairy 중 OAuth2 인증이 필요한 TestCode Class 생성

---
+++ server/src/test/java/com/soothee/dairy/controller/DairyControllerTest.java
@@ -0,0 +1,22 @@
+package com.soothee.dairy.controller;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DairyControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void sendAllDairyMonthly() {
+    }
+}
