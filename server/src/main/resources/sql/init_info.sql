9baa8f0917ab8e1727d6e8b25cd9f3849ed3311e
Juhee Kim <rlawngml59@gmail.com>
2024-11-29 16:40:08 +0900

    Build: 초기화 쿼리에서 daily_score insert 제거

--- server/src/main/resources/sql/init_info.sql
+++ server/src/main/resources/sql/init_info.sql
@@ -27,16 +27,3 @@ VALUES (NULL, 'sunny'),
        (NULL, 'rainy'),
        (NULL, 'thunder'),
        (NULL, 'snow');
-
-INSERT INTO daily_score
-VALUES (NULL, 0),
-       (NULL, 1),
-       (NULL, 2),
-       (NULL, 3),
-       (NULL, 4),
-       (NULL, 5),
-       (NULL, 6),
-       (NULL, 7),
-       (NULL, 8),
-       (NULL, 9),
-       (NULL, 10);
