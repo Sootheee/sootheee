0793cdfba168bc41ca4278280f0c8cfa52aead0b
Juhee Kim <rlawngml59@gmail.com>
2024-12-02 17:20:56 +0900

    Chore: 주석 포멧 수정

--- server/src/main/java/com/soothee/reference/repository/ConditionRepository.java
+++ server/src/main/java/com/soothee/reference/repository/ConditionRepository.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 @Repository
 public interface ConditionRepository extends JpaRepository<Condition, Long> {
     /**
-     * 컨디션 일련번호로 컨디션 조회
+     * 컨디션 일련번호로 컨디션 조회</hr>
      *
      * @param condId Long : 해당 컨디션 일련번호
      * @return Optional<Condition> : 조회한 컨디션 정보 (null 가능)
