030755c645ac26b2695fa4a4c4d829b846f24dd4
Juhee Kim <rlawngml59@gmail.com>
2024-12-03 12:12:37 +0900

    Test: Weather/Condition 조회 TestCode 작성 및 성공

--- server/src/test/java/com/soothee/reference/repository/ConditionRepositoryTest.java
+++ server/src/test/java/com/soothee/reference/repository/ConditionRepositoryTest.java
@@ -1,29 +1,24 @@
 package com.soothee.reference.repository;
 
-import com.soothee.reference.domain.Weather;
+import com.soothee.reference.domain.Condition;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-import java.util.Optional;
-
 @TestPropertySource("classpath:application-test.properties")
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
-class WeatherRepositoryTest {
+class ConditionRepositoryTest {
     @Autowired
-    private WeatherRepository weatherRepository;
+    private ConditionRepository conditionRepository;
 
     @Test
-    void findByWeatherId() {
-        Weather weather = weatherRepository.findByWeatherId(1L).orElseThrow();
-        Assertions.assertThat(weather).isNotNull();
+    void findByCondId() {
+        Condition condition = conditionRepository.findByCondId(1L).orElseThrow();
+        Assertions.assertThat(condition).isNotNull();
     }
 }
---
+++ server/src/test/java/com/soothee/reference/service/WeatherServiceImplTest.java
@@ -0,0 +1,29 @@
+package com.soothee.reference.service;
+
+import com.soothee.reference.domain.Weather;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.transaction.annotation.Transactional;
+
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+@TestPropertySource("classpath:application-test.properties")
+@SpringBootTest
+@Transactional
+@EnableJpaAuditing
+@ActiveProfiles("test")
+class WeatherServiceImplTest {
+    @Autowired
+    private WeatherService weatherService;
+
+    @Test
+    void getWeatherById() {
+        Weather weather = weatherService.getWeatherById(1L);
+        Assertions.assertThat(weather.getWeatherId()).isEqualTo(1L);
+    }
+}
