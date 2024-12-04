f9bf00ccdc3af7021a67807bceb9d1cc4bd255fe
Juhee Kim <rlawngml59@gmail.com>
2024-12-04 17:10:08 +0900

    Feat: DairyCondition Repository에 QueryDSL 추가

--- server/src/main/java/com/soothee/dairy/repository/DairyConditionRepository.java
+++ server/src/main/java/com/soothee/dairy/repository/DairyConditionRepository.java
@@ -2,13 +2,14 @@ package com.soothee.dairy.repository;
 
 import com.soothee.dairy.domain.DairyCondition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface DairyConditionRepository extends JpaRepository<DairyCondition, Long> {
+public interface DairyConditionRepository extends JpaRepository<DairyCondition, Long>, QuerydslPredicateExecutor<DairyCondition>, DairyConditionRepositoryQdsl {
     /**
      * 일기 일련번호로 일기의 컨디션 리스트 조회</hr>
      * 일기 컨디션의 선택 순서대로 조회됨
