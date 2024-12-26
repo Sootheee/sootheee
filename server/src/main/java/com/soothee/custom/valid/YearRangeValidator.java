32d6dc67cc005a128f9b5511e856865534295384
Juhee Kim <rlawngml59@gmail.com>
2024-12-26 23:29:56 +0900

    Feat: Valid Annotation 추가 및 수정

--- server/src/main/java/com/soothee/custom/valid/YearRangeValidator.java
+++ server/src/main/java/com/soothee/custom/valid/YearRangeValidator.java
@@ -4,17 +4,25 @@ import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class YearRangeValidator implements ConstraintValidator<YearRange, LocalDate> {
+    private int minYear;
+    private int maxYear;
+
     @Override
     public void initialize(YearRange constraintAnnotation) {
-        int minYear = constraintAnnotation.minYear();
-        int maxYear = constraintAnnotation.maxYear();
+        this.minYear = constraintAnnotation.minYear();
+        this.maxYear = constraintAnnotation.maxYear();
     }
 
     @Override
     public boolean isValid(LocalDate value, ConstraintValidatorContext constraintValidatorContext) {
-        return !value.isBefore(LocalDate.of(2024, 1, 1))
-                && !value.isAfter(LocalDate.of(2100, 12, 31));
+                /* 입력 값이 null 이 아니고 */
+        return Objects.nonNull(value)
+                /* 입력 값이 2024년 이전이 아니고*/
+                && !value.isBefore(LocalDate.of(minYear, 1, 1))
+                /* 입력 값이 2100년 이후가 아닌지 확인 */
+                && !value.isAfter(LocalDate.of(maxYear, 12, 31));
     }
 }
